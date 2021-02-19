aa9ef326d4f00656e1dd2fe05e0db98c370b382a
Aleah Fredrickson <aleah.fredrickson@gmail.com>
2021-02-19 05:33:50 -0700

    Add unique id method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -65,8 +66,9 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        StringBuilder stringBuilder = new StringBuilder(n);
+        stringBuilder.append(UUID.randomUUID().toString());
+        return stringBuilder.toString();
     }
 
 
