141601eb7b06fa90f5ff74e5ae5734467f08d32e
Aleah Fredrickson <aleah.fredrickson@gmail.com>
2021-02-26 03:01:12 -0700

    Add main interface

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+  private static Calculator calculator;
+
+  public static void main(String[] args) {
+    calculator = new Calculator();
+    int result = 0;
+    String resultStr="";
+
+    switch(args[0]) {
+      case "add":
+        result=calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(result);
+        break;
+      case "subtract":
+        result=calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(result);
+        break;
+      case "multiply":
+        result=calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(result);
+        break;
+      case "divide":
+        result=calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(result);
+        break;
+      case "fibonacciNumberFinder":
+        result=calculator.fibonacciNumberFinder(Integer.parseInt(args[1]));
+        System.out.println(result);
+        break;
+      case "intToBinaryNumber":
+        resultStr=calculator.intToBinaryNumber(Integer.parseInt(args[1]));
+        System.out.println(resultStr);
+        break;
+      case "createUniqueID":
+        resultStr=calculator.createUniqueID(args[1]);
+        System.out.println(resultStr);
+        break;
+    }
+  }
+}
